d86f0e6474ffeb8d95139b6da74520e1d8593d44
Lham <dkeka320@naver.com>
2019-06-18 00:04:37 +0900

    최초 실행

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ moment.tz.setDefault("Asia/Seoul");
 var cron = require('node-cron');
 
 
-
+var curHour = moment().format('HH');
+var numHour = Number(curHour);
+if (parseInt(7) <= parse(numHour)) {
+    sp.write('t');
+    console.debug( curHour + "화장실 불을 켭니다 ");
+} if (parseInt(14) <= parse(numHour)) {
+    sp.write('f');
+    console.debug( curHour + "화장실 불을 끕니다 ");
+}
 
 // second minute hour day-of-month month day-of-week
 cron.schedule('*/30 * * * *', function () {
