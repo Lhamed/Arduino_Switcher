30580e1002a0d6fc146e87c508e0d6929c6b5c4b
Lham <dkeka320@naver.com>
2019-06-18 00:14:48 +0900

    완성

--- index.js
+++ index.js
@@ -9,27 +9,34 @@ moment.tz.setDefault("Asia/Seoul");
 var cron = require('node-cron');
 
 
-var curHour = moment().format('HH');
-var numHour = Number(curHour);
-console.debug( curHour + "시 입니다 ");
-if (parseInt(0) <= parseInt(numHour)) {
-    sp.write('t');
-    console.debug( curHour + "화장실 불을 켭니다 ");
-} if (parseInt(14) <= parseInt(numHour)) {
-    sp.write('f');
-    console.debug( curHour + "화장실 불을 끕니다 ");
-}
+timeCheck();
 
 // second minute hour day-of-month month day-of-week
 cron.schedule('*/30 * * * *', function () {
+    timeCheck();
+});
+
+function timeCheck()
+{
+
     var curHour = moment().format('HH');
     var numHour = Number(curHour);
-    console.debug( curHour + "시 입니다 ");
-    if (parseInt(7) <= parseInt(numHour)) {
+    console.debug(curHour + "시 입니다 ");
+    if (parseInt(0) <= parseInt(numHour) &&
+        parseInt(2) >= parseInt(numHour)) {
         sp.write('t');
-        console.debug( curHour + "화장실 불을 켭니다 ");
-    } if (parseInt(14) <= parseInt(numHour)) {
-        sp.write('f');
-        console.debug( curHour + "화장실 불을 끕니다 ");
+        console.debug(curHour + "화장실 불을 끕니다 ");
     }
-});
+    else if (parseInt(7) <= parseInt(numHour) &&
+        parseInt(10) >= parseInt(numHour)) {
+        sp.write('t');
+        console.debug(curHour + "화장실 불을 켭니다 ");
+    } else if (parseInt(17) <= parseInt(numHour) &&
+        parseInt(23) >= parseInt(numHour)) {
+        sp.write('t');
+        console.debug(curHour + "화장실 불을 켭니다 ");
+    } else {
+        sp.write('f');
+        console.debug(curHour + "화장실 불을 끕니다 ");
+    }
+}
